708b00381295057055c341c6126cc2f947996391
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-07-16 14:42:52 +0900

    init

---
+++ src/test/kotlin/com/dgswalphamales/alphamap/AlphaMapServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dgswalphamales.alphamap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlphaMapServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
